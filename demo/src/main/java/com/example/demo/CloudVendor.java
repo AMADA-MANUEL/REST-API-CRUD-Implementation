a7e967df9b36bca32c2bc293de4c600f4f53f3d5
ammanuejhb024 <you@example.com>
2026-03-06 08:53:35 +0200

    variables for cloud vendor

---
+++ demo/src/main/java/com/example/demo/CloudVendor.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class CloudVendor {
+    private String vendorId;
+    private String vendorName;
+    private String vendorAddress;
+    private String vendorPhone;
+}
