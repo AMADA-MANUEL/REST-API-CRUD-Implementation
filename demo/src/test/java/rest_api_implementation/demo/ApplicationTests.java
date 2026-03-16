07664ef25a685ddb314f49befb30cd2cd43b3321
ammanuejhb024 <manuelamada707@gmail.com>
2026-03-16 21:28:26 +0200

    initial commit

---
+++ demo/src/test/java/rest_api_implementation/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rest_api_implementation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
